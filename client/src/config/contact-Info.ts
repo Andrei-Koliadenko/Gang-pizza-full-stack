379b6696b6fcde8889f4db6085c24dce88ddfa64
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-21 20:41:27 +0200

    Update Footer

--- client/src/config/contact-Info.ts
+++ client/src/config/contact-Info.ts
@@ -1,4 +1,10 @@
-export const pizzeria_name: string = "Gang Pizza";
-export const phoneNumber: string = "+972 053 777-55-99";
-export const email: string = "gang-pizza@gmail.com";
+export const PIZZERIA_NAME: string = "Gang Pizza";
+export const PIZZERIA_NAME_CAPS: string = "GANG PIZZA";
+export const PIZZERIA_PHONE_NUMBER: string = "+972 053 777-55-99";
+export const PIZZERIA_EMAIL: string = "gang-pizza@gmail.com";
+
+//Social network links
+export const FACEBOOK_LINK: string = 'https://www.facebook.com/';
+export const INSTAGRAM_LINK: string = 'https://instagram.com/';
+export const LINKEDIN_LINK: string = 'https://www.linkedin.com/';
 
